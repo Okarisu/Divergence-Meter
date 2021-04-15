1c920bb5a6f4962c3baf37b8eff3186391f2f7db
Okarisu <okarisu@protonmail.com>
2021-04-15 14:50:50 +0200

    Update meter.ino

--- meter.ino
+++ meter.ino
@@ -1,8 +1,13 @@
 
+
+#include <SoftwareSerial.h>
 #define RX 11
 #define TX 10
-#define pinLED 13
-#include <SoftwareSerial.h>
+
+#include "LedControl.h"
+#define DIN 12
+#define CS 9
+#define CLK 13
 
 boolean scramble = false;
 boolean alpha = false;
@@ -12,10 +17,13 @@ SoftwareSerial bluetooth(TX, RX);
 void setup()
 {
     bluetooth.begin(9600);
+    setChar(0, 6, ., false);
 }
 
-void logic()
+void loop()
 {
+
+    //BT logic
     byte BluetoothData;
 
     if (bluetooth.available() > 0)
@@ -40,5 +48,35 @@ void logic()
             bluetooth.println("Invalid command.");
         }
     }
-    delay(100);
+
+    //call fc
+
+    if (scramble)
+    {
+        scramble();
+        scramble = false;
+    }
+    if (alpha)
+    {
+        alpha();
+        alpha = false;
+    }
+    if (beta)
+    {
+        beta();
+        beta = false;
+    }
+}
+
+void scramble()
+{
+    setDigit(0, 7, 1, false);
+}
+void alpha()
+{
+    setDigit(0, 7, 0, false);
+}
+void beta()
+{
+    setDigit(0, 7, 1, false);
 }
